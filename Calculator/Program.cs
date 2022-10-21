447079942a2b8d658f2bd05d341e54e373ccbad4
Joelson Freitas de Oliveira <joelsonfoliveira@gmail.com>
2022-10-21 09:14:14 -0300

    Iniciando o projeto

---
+++ Calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Calculator
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      float primeiroValor = 0.0f;
+
+      Console.Clear();
+      Console.Write("Entre com o primeiro valor: ");
+
+      primeiroValor = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("Primeiro Valor: " + primeiroValor);
+    }
+  }
+}
