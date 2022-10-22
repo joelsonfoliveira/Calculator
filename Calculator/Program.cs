6d7c7f96ff39d1424ad1616846c35106965b35a3
Joelson Freitas de Oliveira <joelsonfoliveira@gmail.com>
2022-10-22 11:10:19 -0300

    Divisão

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,6 +8,7 @@ namespace Calculator
     {
       Somar();
       Subtrair();
+      Dividir();
     }
 
     static void Somar()
@@ -46,6 +47,27 @@ namespace Calculator
       segundoValor = float.Parse(Console.ReadLine());
 
       resultado = primeiroValor - segundoValor;
+
+      Console.WriteLine($"Resultado: {resultado}");
+
+      Console.ReadKey();
+    }
+
+    static void Dividir()
+    {
+      float primeiroValor = 0.0f;
+      float segundoValor = 0.0f;
+      float resultado = 0.0f;
+
+      Console.Clear();
+
+      Console.Write("Entre com o primeiro valor: ");
+      primeiroValor = float.Parse(Console.ReadLine());
+
+      Console.Write("Entre com o segundo valor: ");
+      segundoValor = float.Parse(Console.ReadLine());
+
+      resultado = primeiroValor / segundoValor;
 
       Console.WriteLine($"Resultado: {resultado}");
 
