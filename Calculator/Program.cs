218148d75d071170b789bd2305467e9d8b6e159a
Joelson Freitas de Oliveira <joelsonfoliveira@gmail.com>
2022-10-22 08:54:31 -0300

    soma

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,13 +7,20 @@ namespace Calculator
     public static void Main(string[] args)
     {
       float primeiroValor = 0.0f;
+      float segundoValor = 0.0f;
+      float resultado = 0.0f;
 
       Console.Clear();
-      Console.Write("Entre com o primeiro valor: ");
 
+      Console.Write("Entre com o primeiro valor: ");
       primeiroValor = float.Parse(Console.ReadLine());
 
-      Console.WriteLine("Primeiro Valor: " + primeiroValor);
+      Console.Write("Entre com o segundo valor: ");
+      segundoValor = float.Parse(Console.ReadLine());
+
+      resultado = primeiroValor + segundoValor;
+
+      Console.WriteLine($"Soma: {resultado}");
     }
   }
 }
