1716b8035150cbb0f6b2647f925d6c694c764eb1
Joelson Freitas de Oliveira <joelsonfoliveira@gmail.com>
2022-10-22 11:11:14 -0300

    multiplicação

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -73,5 +73,26 @@ namespace Calculator
 
       Console.ReadKey();
     }
+
+    static void Multiplicar()
+    {
+      float primeiroValor = 0.0f;
+      float segundoValor = 0.0f;
+      float resultado = 0.0f;
+
+      Console.Clear();
+
+      Console.Write("Entre com o primeiro valor: ");
+      primeiroValor = float.Parse(Console.ReadLine());
+
+      Console.Write("Entre com o segundo valor: ");
+      segundoValor = float.Parse(Console.ReadLine());
+
+      resultado = primeiroValor * segundoValor;
+
+      Console.WriteLine($"Resultado: {resultado}");
+
+      Console.ReadKey();
+    }
   }
 }
